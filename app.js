b0837a21713692331c2b558981c9fd216cbb59a0
Aliaksandr Fedukovich <1632637@mail.ru>
2023-04-23 19:50:23 +0300

    fix(basic-3.1): one style for id names

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("newTask");//Add a new task.
+var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("todo-task");//ul of #todo-task
-var completedTasksHolder=document.getElementById("completedTasks");//completedTasks
+var completedTasksHolder=document.getElementById("task-completed");//task-completed
 
 
 //New task list item
@@ -65,7 +65,7 @@ var createNewTaskElement=function(taskString){
 
 var addTask=function(){
     console.log("Add Task...");
-    //Create a new list item with the text from the #newTask:
+    //Create a new list item with the text from the #new-task:
     if (!taskInput.value) return;
     var listItem=createNewTaskElement(taskInput.value);
 
@@ -123,7 +123,7 @@ var deleteTask=function(){
 var taskCompleted=function(){
     console.log("Complete Task...");
 
-    //Append the task list item to the #completedTasks
+    //Append the task list item to the #task-completed
     var listItem=this.parentNode;
     completedTasksHolder.appendChild(listItem);
     bindTaskEvents(listItem, taskIncomplete);
